00fbd553f7cc098bd29204423b68cbe38f3f29cf
Vladislav Flinski <flinssski@gmail.com>
2025-03-03 18:01:02 +0300

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const copy = {};
+  return Object.assign(copy, obj);
 }
 
 /**
@@ -32,8 +33,18 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((resObj, curObj) => {
+    const resCopyObj = resObj;
+    Object.entries(curObj).forEach(([key, value]) => {
+      if (key in resObj) {
+        resCopyObj[key] += value;
+      } else {
+        resCopyObj[key] = value;
+      }
+    });
+    return resCopyObj;
+  }, {});
 }
 
 /**
